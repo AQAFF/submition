915dd5bfa648ee6a4339741cd64eac230935a266
TUF <TUF@LAPTOP-257ULQGP>
2022-04-22 01:25:16 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intSum As Integer
+
+
+        If (Val(TextBox1.Text) > 0) Then
+            intSum = 0
+            For x = 1 To Val(TextBox1.Text)
+                intSum = intSum + x
+
+            Next x
+            Label2.Text = "Summation of " & TextBox1.Text & " is " & Str(intSum) & "."
+        Else
+            Label2.Text = "Enter a positive integer only."
+
+        End If
+    End Sub
+End Class
